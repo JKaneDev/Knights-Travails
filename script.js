e1068bc6dcf72895b251970d158d8c77296eb6c2
James Kane <james.kane59@outlook.com>
2023-01-17 17:37:40 +0000

    create array of possible moves by adding current x,y with all possible knight moves

--- script.js
+++ script.js
@@ -6,17 +6,44 @@ function createSquare(x, y) {
 	let prev;
 
 	// array list of knight moves
+	const knightMoves = [
+		[1, 2],
+		[2, 1],
+		[-1, 2],
+		[2, -1],
+		[-2, 1],
+		[1, -2],
+		[-1, -2],
+		[-2, -1],
+	];
 
 	// set prev square
+	const setPrevSquare = () => prev;
 	// get prev square
+	const getPrevSquare = (newPrev) => {
+		prev = prev || newPrev;
+	};
 
 	// variable: args square coords
+	const coords = () => `${x}, ${y}`;
 
 	// function: return array of valid squares knight can move to from current square
+	const createPossibleMoves = () => {
+		return knightMoves
+			.map((offset) => generateOptions(offset[0], offset[1]))
+			.filter((square) => square != undefined);
+	};
 
 	// function: returns new square object if within board bounds
 	// adds knight move offset coords to current square coords
 	// recursively calls create square on new coords
+	const generateOptions = (xOffset, yOffset) => {
+		const [newX, newY] = [xPos + xOffset, yPos + yOffset];
+		// check if possible moves are in board bounds
+		if (0 <= newX && newX < 8 && 0 <= newY && newY <= 8) {
+			return createSquare(newX, newY);
+		}
+	};
 
 	// if square already visited: return coords for this function call
 	// if not: add new square to visited map
